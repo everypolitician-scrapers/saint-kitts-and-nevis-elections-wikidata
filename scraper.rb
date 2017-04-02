8d1158c1489e1da7bd8a0ba441bb0d0e67381c75
Tony Bowden <tony@tmtm.com>
2017-04-02 12:54:20 +0100

    Also get P39 Wikidata members

--- scraper.rb
+++ scraper.rb
@@ -6,4 +6,7 @@ require 'wikidata/fetcher'
 page_names = EveryPolitician::Wikidata.morph_wikinames(source: 'tmtmtmtm/saint-kitts-and-nevis-elections-wikipedia', column: 'wikiname')
 cat_names = WikiData::Category.new('Category:Members of the National Assembly (Saint Kitts and Nevis)', 'en').member_titles
 
-EveryPolitician::Wikidata.scrape_wikidata(names: { en: page_names | cat_names }, output: false)
+sparq = 'SELECT ?item WHERE { ?item wdt:P39 wd:Q21295997 . }'
+p39s  = EveryPolitician::Wikidata.sparql(sparq)
+
+EveryPolitician::Wikidata.scrape_wikidata(ids: p39s, names: { en: page_names | cat_names })
